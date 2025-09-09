d9b7b4ff9898bfcc3c4aa9133c889321aec4b407
sophis <sophiacostapittner@gmail.com>
2025-09-09 14:18:17 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("VENDA DE BICHOS ILEGAIS")
+st.caption ("***PINGUIM DO SAARA A VENDA TROCO POR DOIS PULMÔES***")
+st.image("https://th.bing.com/th/id/OIP.8l4qoAExcnX3jhXzHMa74AHaE1?w=304&h=199&c=7&r=0&o=7&pid=1.7&rm=3")
+
+st.markdown(
+"""
+### **TRANSPLANTE**
+""")
+st.video("https://youtu.be/PHKCQ7t9y5E?si=7cGM6k-ScDZ8amjb")
